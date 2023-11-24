8df91496b279dc33714a23adb303e14a0b107546
NastasiaMood <150917861+NastasiaMood@users.noreply.github.com>
2023-11-24 14:37:50 +0500

    Add files via upload
    подключила Prettier Formatter, настроила автоматическое форматирование при сохранении
    
    тег <head>: 
    подключила стиль с помощью link
    добавила заголовок страницы - title
    c тегом meta передаем следующую информацию: 
                 charset="UTF-8" - кодировка символов для HTML документа
                      UTF-8 - самая распространенная совр. кодировка
                 name="viewport" content="width=device-width, initial-scale=1.0"
                      viewport - контроль масштаба отображения страницы
                      initial-scale — коэффициент масштабирования
                      width=device-width ширина видимой области = ширине устройства
                      initial-scale=1.0 - начального масштаба нет
    
    без функций

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,47 +1,85 @@
-const input = document.querySelector('#input')
-const btn = document.querySelector('#btn')
-const result = document.querySelector('#result')
-const total = document.querySelector('#total')
-let i=0
+/* было
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+const total = document.querySelector("#total");
+let i = 0; */
 
+//DOM-элементы
+const todoInput= document.querySelector ("#todoInput")
+const todoBtn = document.querySelector ("#todoBtn")
+const userRandBtn = document.querySelector ("#userRandBtn")
+const Result = document.querySelector ("#Result")
+const userRandList = document.querySelector ("#userRandList")
+const Filter = document.querySelector ("#Filter")
+
+//Слушатели событий
+document.addEventListener("DOMContentLoaded", getTodos);
+todoBtn.addEventListener("click", addTodo);
+userRandBtn.addEventListener("click", gerRandUsers);
+Result.addEventListener("click", deleteTodo);
+Filter.addEventListener("click", filterTodo);
+
+
+//функции
+function addTodo (e) {
+  //e.preventDefault();
+  
+}
+
+function deleteTodo(e) {
+}
+
+function filterTodo(e) {
+}
+
+function saveLocalTodos(todo) {
+}
+
+function getTodos() {
+}
+
+function getRandomUsers() {
+}
+
+/* 
 //add event добавление события на кнопку
-btn.addEventListener('click', (e)=>{
-    console.log(input.value)
+btn.addEventListener("click", (e) => {
+  console.log(input.value);
 
-    //result.innerHTML += '<li>${input.value}<li>'
-    if(input.value === '')return
-    createDeleteElements(input.value)
-    input.value = ''
-})
+  //result.innerHTML += '<li>${input.value}<li>'
+  if (input.value === "") return;
+  createDeleteElements(input.value);
+  input.value = "";
+});
 
 //создание и удаление
-function createDeleteElements(value)
-{
-    //создание активного класса
-    i++
-    const li = document.createElement('li')
-    const btn = document.createElement('button')
+function createDeleteElements(value) {
+  //создание активного класса
+  i++;
+  const li = document.createElement("li");
+  const btn = document.createElement("button");
 
-    li.className = 'li'
-    li.textContent=value
-    
-    btn.className = 'btn'
-    btn.textContent='add'
-    li.appendChild(btn)
+  li.className = "li";
+  li.textContent = value;
 
-    //удаление активного класса
-    btn.addEventListener('click', (e)=>{
-        i--
-        total.textContent=i
-        result.removeChild(li)
-        })
+  btn.className = "btn";
+  btn.textContent = "add";
+  li.appendChild(btn);
 
-    //переключение активного класса
-    li.addEventListener('click', (e)=>{
-        li.classList.toggle('li-active')
-    })
+  //удаление активного класса
+  btn.addEventListener("click", (e) => {
+    i--;
+    total.textContent = i;
+    result.removeChild(li);
+  });
 
-    total.textContent=i
+  //переключение активного класса
+  li.addEventListener("click", (e) => {
+    li.classList.toggle("li-active");
+  });
 
-    result.appendChild(li) 
-}
+  total.textContent = i;
+
+  result.appendChild(li);
+} */
